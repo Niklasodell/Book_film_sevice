c0afe973564fd3b3be94d324825ab0c6f0d2f1c7
Niklasodell <144327353+Niklasodell@users.noreply.github.com>
2024-11-20 16:18:34 +0100

    Spring Initializr generated

---
+++ src/main/java/com/example/bookfilmservice/BookfilmserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookfilmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookfilmserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookfilmserviceApplication.class, args);
+	}
+
+}
